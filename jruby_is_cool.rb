4849f3c57ee2722f2901a184423a4a8e3a00f5f8
myappleguy <myappleguy@gmail.com>
2013-03-16 21:32:36 -0500

    added ruby file

---
+++ jruby_is_cool.rb
@@ -0,0 +1,4 @@
+# just needed something to do while I am waiting for vagrant and git on other project
+puts "Hey Buddy!"
+# this remods me that i need to check Wibit.net again, I havent seen it in a while, heard it was down for some reworking
+
